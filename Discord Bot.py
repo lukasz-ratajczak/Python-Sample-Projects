d57a611a5f98f28924b706d08ece2b3b8fcbea5a
lukasz <l.m.ratajczak94@gmail.com>
2022-08-02 22:07:17 +0200

    Discord Bot refactor + hello

--- Discord Bot.py
+++ Discord Bot.py
@@ -4,17 +4,30 @@
 import json, discord
 from discord.ext import commands
 
-# Discord Token - hidden key
+# Discord hidden keys
 with open('misc.json') as f:
     token = json.load(f)['api_keys']['discord_bot_token']
+    channel_id = json.load(f)['discord_bot']['channel_id']
 
-# Bot setup
-client = commands.Bot("#!")
+# Bot Setup
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix="^", intents=intents)
 
 @client.event
 async def on_ready():
     print("Bot ready!")
     print("----------")
+
+# Bot Events
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(channel_id)
+    await channel.send("Hello "+member)
+
+# Bot Commands
 
 @client.command()
 async def hello(ctx):
